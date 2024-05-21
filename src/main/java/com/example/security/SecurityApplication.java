5c840fdf4ee0909eafe0654a98046d0470526301
soman-maharjan <maharjansoman@yahoo.com>
2024-05-21 17:12:52 +0545

    TD-0003: Task Scheduler > Implements Task Scheduler using H2 in-memory database

--- src/main/java/com/example/security/SecurityApplication.java
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -2,11 +2,13 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 }
--- src/main/java/com/example/security/helpers/EncryptionHelper.java
+++ src/main/java/com/example/security/helpers/EncryptionHelper.java
@@ -1,8 +1,6 @@
 package com.example.security.helpers;
 
-import lombok.NonNull;
-import lombok.SneakyThrows;
-
+import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -10,7 +8,8 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.SecureRandom;
+import lombok.NonNull;
+import lombok.SneakyThrows;
 
 public class EncryptionHelper {
 
@@ -28,17 +27,30 @@ public class EncryptionHelper {
     }
 
     @SneakyThrows
-    public static SecretKey generateKey(@NonNull char[] password, @NonNull byte[] salt) {
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+    public static SecretKey generateKey(
+        @NonNull char[] password,
+        @NonNull byte[] salt
+    ) {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(
+            "PBKDF2WithHmacSHA256"
+        );
         int keySizeBits = KEY_SIZE * 8;
-        PBEKeySpec keySpec = new PBEKeySpec(password, salt, KEY_ITERATION_COUNT, keySizeBits);
+        PBEKeySpec keySpec = new PBEKeySpec(
+            password,
+            salt,
+            KEY_ITERATION_COUNT,
+            keySizeBits
+        );
         SecretKey temporaryKey = factory.generateSecret(keySpec);
         keySpec.clearPassword();
         return new SecretKeySpec(temporaryKey.getEncoded(), "AES");
     }
 
     @SneakyThrows
-    public static byte[] encrypt(@NonNull SecretKey key, @NonNull byte[] clearText) {
+    public static byte[] encrypt(
+        @NonNull SecretKey key,
+        @NonNull byte[] clearText
+    ) {
         byte[] ivBytes = new byte[IV_SIZE];
         RANDOM.nextBytes(ivBytes);
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -48,7 +60,10 @@ public class EncryptionHelper {
     }
 
     @SneakyThrows
-    public static byte[] decrypt(@NonNull SecretKey key, @NonNull byte[] cipherText) {
+    public static byte[] decrypt(
+        @NonNull SecretKey key,
+        @NonNull byte[] cipherText
+    ) {
         byte[][] byteArrays = split(cipherText);
         byte[] ivBytes = byteArrays[0];
         byte[] cipherBytes = byteArrays[1];
@@ -58,9 +73,16 @@ public class EncryptionHelper {
     }
 
     private static byte[] concat(byte[] ivBytes, byte[] cipherBytes) {
-        byte[] concatenatedBytes = new byte[ivBytes.length + cipherBytes.length];
+        byte[] concatenatedBytes = new byte[ivBytes.length +
+        cipherBytes.length];
         System.arraycopy(ivBytes, 0, concatenatedBytes, 0, ivBytes.length);
-        System.arraycopy(cipherBytes, 0, concatenatedBytes, ivBytes.length, cipherBytes.length);
+        System.arraycopy(
+            cipherBytes,
+            0,
+            concatenatedBytes,
+            ivBytes.length,
+            cipherBytes.length
+        );
         return concatenatedBytes;
     }
 
@@ -68,8 +90,13 @@ public class EncryptionHelper {
         byte[] ivBytes = new byte[IV_SIZE];
         byte[] cipherBytes = new byte[concatenatedBytes.length - IV_SIZE];
         System.arraycopy(concatenatedBytes, 0, ivBytes, 0, IV_SIZE);
-        System.arraycopy(concatenatedBytes, IV_SIZE, cipherBytes, 0, concatenatedBytes.length - IV_SIZE);
-        return new byte[][]{ivBytes, cipherBytes};
+        System.arraycopy(
+            concatenatedBytes,
+            IV_SIZE,
+            cipherBytes,
+            0,
+            concatenatedBytes.length - IV_SIZE
+        );
+        return new byte[][] { ivBytes, cipherBytes };
     }
-
 }
